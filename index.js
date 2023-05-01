cf7b152f4095957e44c1e78ca98c0a30a80a354d
Mahedi3110 <shuvo@shuvo>
2023-05-02 03:35:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const allChefs = require('./Chef.json');
+const cors = require('cors');
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/allChef', (req, res) => {
+    res.send(allChef)
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = allChefs.find(data => data._id == id)
+    res.send(chef)
+})
+
+app.get('/food/:id', (req, res) => {
+    const chefData = []
+    const allFood = []
+    const id = req.params.id;
+    allChefs.map(chef => chefData.push(chef._bestFood))
+    chefData[0].map(food => allFood.push(food))
+    chefData[1].map(food => allFood.push(food))
+    chefData[2].map(food => allFood.push(food))
+    chefData[3].map(food => allFood.push(food))
+    chefData[4].map(food => allFood.push(food))
+    chefData[5].map(food => allFood.push(food))
+    chefData[6].map(food => allFood.push(food))
+    chefData[7].map(food => allFood.push(food))
+    chefData[8].map(food => allFood.push(food))
+    // const filtered = allChefs.map(chef => chef._bestFood.find(data => data.id == id))
+    const findData = allFood.find(data => data.id == id)
+    res.send(findData)
+})
+
+app.listen(port, () => [
+    console.log(`website is running on port: ${port}`)
+])
